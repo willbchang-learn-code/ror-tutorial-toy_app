d1135bc01a886844f225f87ce8714e87d09b61cd
Will B Chang <willbeethoven@gmail.com>
2020-09-10 01:22:56 +0000

    chore: Allow Cloud9 connetions.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Aloow Cloud9 connetions.
+  config.hosts.clear
 end
