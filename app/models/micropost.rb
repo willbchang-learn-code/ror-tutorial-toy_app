cfacf153fe3629398b0aa98ab8615715c85f314b
Will B Chang <willbeethoven@gmail.com>
2020-09-10 03:39:46 +0000

    feat: make micropost belogs to user

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ApplicationRecord
+  belongs_to :user
   validates :content, length: { maximum: 140 }
   validates :content, length: { minimum: 1 }
   validates :user_id, length: { minimum: 1}
